01505041089eac8bdd93db3ce2f59ec2553ae839
Akira <junrongfu6@gmail.com>
2024-03-08 21:36:38 +0800

    frequency counter pattern

--- max_sum_refactored.js
+++ max_sum_refactored.js
@@ -1,4 +1,4 @@
-function maxSubarraySum(arr, num){
+function maxSubarraySum(arr, num) {
   let maxSum = 0;
   let tempSum = 0;
   if (arr.length < num) return null;
@@ -9,8 +9,10 @@ function maxSubarraySum(arr, num){
   for (let i = num; i < arr.length; i++) {
     tempSum = tempSum - arr[i - num] + arr[i];
     maxSum = Math.max(maxSum, tempSum);
+    console.log(tempSum, maxSum);
   }
+
   return maxSum;
 }
 
-maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
+maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3);
